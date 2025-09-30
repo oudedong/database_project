72fde66336995e9ef3985d11a55c85f78c91dfea
oudedong <hamhs1018@gamil.com>
2025-09-30 21:47:13 +0900

    fix logic

--- tree_puzzle/core/models.py
+++ tree_puzzle/core/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Result(models.Model):
+    gameId = models.BigAutoField(primary_key=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    time   = models.IntegerField()
-    score  = models.IntegerField()
+    time   = models.IntegerField(null=True)
+    score  = models.IntegerField(null=True)
+    date   = models.DateField(default=datetime.date.today)
